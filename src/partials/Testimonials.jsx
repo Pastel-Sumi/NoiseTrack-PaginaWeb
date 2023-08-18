9e204e52efdaf0a59a7ac59c3bdfe65c0f612480
Esteban Manríquez <esteban.manriquez@sansano.usm.cl>
2023-08-17 23:49:52 -0400

    Página web

---
+++ src/partials/Testimonials.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+
+import TestimonialImage01 from '../images/testimonial-01.jpg';
+import TestimonialImage02 from '../images/testimonial-02.jpg';
+import TestimonialImage03 from '../images/testimonial-03.jpg';
+
+import BastianImage from '../images/bastian.jpg';
+import CamilaImage from '../images/camila.jpg';
+import DiegoImage from '../images/diego.jpg';
+import EstebanImage from '../images/esteban.jpg';
+import RobertoImage from '../images/roberto.jpg';
+import SofiaImage from '../images/sofia.jpg';
+
+function Testimonials() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4"> ¿Quiénes somos?</h2>
+            <p className="text-xl text-gray-400">Roku6its es una empresa fundada en la Universidad Federico Santa María por un grupo de seis estudiantes de Ingeniería Civil Informática. Surgimos como resultado de la Feria de Software de la universidad, y nos enorgullecemos de llevar la innovación y la tecnología a nuevos horizontes. <br/>
+            </p>
+            <br/>
+            
+          </div>
+
+          {/* Miembros */}
+          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-2 lg:gap-6 items-start lg:max-w-none">
+
+            {/* Camila */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img  className="rounded-full" src={CamilaImage} width="80" height="80" alt="Camila Arancibia" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                        </div>
+                </div>
+                <blockquote className="text-lg text-gray-400 grow">— La jefa.</blockquote>
+                <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                  <cite className="text-gray-200 not-italic">Camila Arancibia</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/camila-arancibia/" target='_blank'>Scrum Master</a>
+                </div>
+              </div>
+
+            {/* Sofia */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="200">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={SofiaImage} width="80" height="80" alt="Sofia Martínez - Product Owner" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                      </div>
+                </div>
+                <blockquote className="text-lg text-gray-400 grow">— La segunda jefa.</blockquote>
+                <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                  <cite className="text-gray-200 not-italic">Sofia Martínez</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/sofia-martinez-alvarez/" target='_blank'>Product Owner</a>
+                </div>
+              </div>
+                
+            {/* Diego */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={DiegoImage} width="80" height="80" alt="Diego Aguilera - Marketing" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                      </div>
+                  </div>
+                  <blockquote className="text-lg text-gray-400 grow">— Marketeo.</blockquote>
+                  <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                   <cite className="text-gray-200 not-italic">Diego Aguilera</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/diegoaguileram/" target='_blank'> Marketing y Comunicaciones</a>
+                </div>
+              </div>
+
+            {/* Roberto */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={RobertoImage} width="80" height="80" alt="Roberto Covarrubias - Testing" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                      </div>
+                    </div>
+                    <blockquote className="text-lg text-gray-400 grow">— Hacer pruebas.</blockquote>
+                    <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                      <cite className="text-gray-200 not-italic">Roberto Covarrubias</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/roberto-covarrubias-figueroa-739843258/" target='_blank'> Encargado de Testing</a>
+                </div>
+              </div>
+
+            {/* Bastian */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={BastianImage} width="80" height="80" alt="Bastian Lara - Encargado de Tecnologías" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                    </svg>
+                      </div>
+                    </div>
+                    <blockquote className="text-lg text-gray-400 grow">— Hombre IA.</blockquote>
+                    <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                      <cite className="text-gray-200 not-italic">Bastian Lara</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/bastian-lara-q/" target='_blank'> Encargado de Tecnologías</a>
+                </div>
+              </div>
+
+            {/* Esteban */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <img className="rounded-full" src={EstebanImage} width="80" height="80" alt="Esteban Manríquez - Encargado UX" />
+                    <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                      </svg>
+                        </div>
+                    </div>
+                    <blockquote className="text-lg text-gray-400 grow">— Hacer la página.</blockquote>
+                    <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                      <cite className="text-gray-200 not-italic">Esteban Manríquez</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/esmanri/" target='_blank'> Encargado UX</a>
+                </div>
+              </div>
+          </div>
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <br/>
+            <p className='text-xl text-gray-400'>Estamos comprometidos a mejorar la salud y seguridad de los trabajadores, y creemos que una de las formas de hacerlo es reducir su exposición al ruido. Los trabajadores que están expuestos al ruido regularmente tienen un mayor riesgo de sufrir pérdida auditiva, tinnitus, problemas de equilibrio y concentración, y estrés. El ruido también puede aumentar el riesgo de accidentes.</p>
+
+            <br/>
+            </div>
+                </div>
+
+              </div>
+
+</section>
+  );
+}
+
+export default Testimonials;
